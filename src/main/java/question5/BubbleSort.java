4bb8e02ec469396f46bc72fbd5ce4d2a76c50145
Olga Karpenko <okarpenko@usfca.edu>
2024-02-29 00:01:27 -0800

    Added starter code for the practice session 1.

---
+++ src/main/java/question5/BubbleSort.java
@@ -0,0 +1,15 @@
+package question5;
+
+/**
+ * Fill in the missing code of the bubble sort so that it sorts the subarray from index low to index arr.length-1 in descending order.
+ * For instance, if the array is {4, 19, 0, 3, 6, 2, 8} and low = 2, the result should be: 4, 19, 8, 6, 3, 2, 0.
+ *
+ * Submit your solution to github (so that you know how to do it during the actual exam).
+ */
+public class BubbleSort {
+    public static void bubbleSortSubarrayDescending(int[] arr, int low) {
+           // FILL IN CODE:
+
+
+    }
+}
---
+++ src/main/java/question7/IntersectionOfSortedArrays.java
@@ -0,0 +1,19 @@
+package question7;
+
+/* Write a function that takes two sorted arrays without duplicates and
+prints all the elements that occur in both arrays.
+For instance, if we pass the following two sorted arrays to the method:
+   int[] arr1= {2, 10, 12, 34, 90};
+   int[] arr2 = {4, 6, 8, 10, 11, 12, 14, 20, 26, 30, 34, 48, 50};
+   then the method should print 10 12 34
+   Note: your method should be general and work for any two sorted arrays of integers. It is required that it runs in linear time: Theta(n1+n2), where n1 and n2 are the sizes of the two lists and takes advantages of the fact that the input lists are sorted.
+
+   Please submit your solution to github.
+ */
+public class IntersectionOfSortedArrays {
+    public static void occurInBothArrays(int[] sortedA, int[] sortedB) {
+        // FILL IN CODE
+
+
+    }
+}
